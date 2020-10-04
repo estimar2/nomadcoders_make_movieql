07282b3f7c91bbfee1180f76bc8279f9d7278fbf
song <songsm5504@gmail.com>
2020-10-04 21:48:16 +0900

    #0.9 Defining Mutations

--- graphql/db.js
+++ graphql/db.js
@@ -1,13 +1,25 @@
-export const people = [
-  { id: "0", name: "Nicolas", age: 19, gender: "female" },
-  { id: "1", name: "song", age: 26, gender: "female" },
-  { id: "2", name: "in", age: 31, gender: "male" },
-  { id: "3", name: "he", age: 25, gender: "female" },
-  { id: "4", name: "ho", age: 22, gender: "male" },
-  { id: "5", name: "Dall", age: 18, gender: "male" },
+let movies = [
+  { id: 0, name: "Star Wars - The new one", score: 1 },
+  { id: 1, name: "Avengers - The new one", score: 9 },
+  { id: 1, name: "The Godfather I", score: 99 },
+  { id: 3, name: "Logan", score: 2 },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === String(id));
-  return filteredPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === String(id));
+
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== String(id));
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+
+    return true;
+  } else {
+    return false;
+  }
 };
