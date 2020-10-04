b42b2590f0ae0a1c26f9e25d01770ec7ec5184b5
song <songsm5504@gmail.com>
2020-10-04 21:57:50 +0900

    #0.10 Creating first Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -15,6 +15,7 @@ export const getById = (id) => {
 
 export const deleteMovie = (id) => {
   const cleanedMovies = movies.filter((movie) => movie.id !== String(id));
+
   if (movies.length > cleanedMovies.length) {
     movies = cleanedMovies;
 
@@ -22,4 +23,16 @@ export const deleteMovie = (id) => {
   } else {
     return false;
   }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+
+  movies.push(newMovie);
+
+  return newMovie;
 };
